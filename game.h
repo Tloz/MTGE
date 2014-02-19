b7c110887383c4e0d671787a4cc6f6f4f7940971
Tloz <scrimet@hotmail.fr>
2014-02-19 09:17:04 +0100

    declared a bunch of functions for the player

--- game.h
+++ game.h
@@ -6,6 +6,15 @@
 
 class Card;
 
+/*
+ * This is the main game aera
+ * wich is supposed to contain:
+ * - Each players' mana pool
+ * - Each players' creatures,
+ *                 artifacts,
+ *                 enchatments.
+ *  Dunno how to implement it yet...
+ */
 class Battlefield
 {
     
@@ -13,10 +22,18 @@ class Battlefield
 
 class Player
 {
-    uint16_t m_life;
+    uint8_t m_life;
     std::vector<Card> m_graveyard;
     std::deque<Card> m_library;
     std::deque<Card> m_hand;
+
+    void draw(int number = 1);
+    void discardFromHand(int number = 1);
+    void discardFromLibrary(int number = 1);
+    void shuffleLibrary();
+    void shuffleHand();
+    void putOnTopOfLibrary(Card card);
+    void putOnBottomOfLibrary(Card card);
 };
 
 class Game
