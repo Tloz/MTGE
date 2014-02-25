63def41b74f5ae640f2f6b8c472fb56482ca6b09
Tloz <scrimet@hotmail.fr>
2014-02-25 19:49:25 +0100

    created Enchantment class

--- cards.h
+++ cards.h
@@ -3,9 +3,9 @@
 #include <cstdint>
 #include <iostream>
 
-enum class Color{None, Black, Blue, Green, Red, White};
+enum class Color{None = 0, Black = 1, Blue = 2, Green = 4, Red = 8, White = 16};
 enum class Mana{Unco, Isle, Mountain, Swamp, Forest, Plain};
-enum class Variety{Common, Unco, Rare};
+enum class Variety{Common, Unco, Rare, Mythic};
 
 class Player;
 
@@ -20,7 +20,6 @@ class Card
     bool taped;
     bool permanent;
     std::string name;
-    std::string illustrator;
     Variety variety;
     bool funcard;
     /* Color color; Do we really need this?*/
@@ -29,7 +28,12 @@ class Card
 
 };
 
-class Land : public Card
+class Permanent
+{
+
+};
+
+class Land : public Card, public Permanent
 {
     
 };
@@ -40,21 +44,37 @@ class Spell : public Card
     std::array<uint8_t, 6> cost;
 };
 
-class Creature : public Spell
+class Creature : public Spell, public Permanent
 {
     std::string type;
     uint8_t atk;
     uint8_t def;
     
-    void attack(Player p);
+    void attack(Player *p);
+    void block(Creature *c)
 };
 
-class Artifact : public Spell
+class Artifact : public Spell, public Permanent
 {
 
 };
 
-class CreatureArtifact : public Creature, public Artifact
+class CreatureArtifact : public Creature, public Artifact, public Permanent
+{
+
+};
+
+class Enchantment : public Spell, public Permanent
+{
+
+};
+
+class Instant: : public Spell
+{
+
+};
+
+class Sorcery : public Spell
 {
 
 };
